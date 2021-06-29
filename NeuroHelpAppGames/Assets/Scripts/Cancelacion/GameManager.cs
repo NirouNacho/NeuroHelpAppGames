7087b015cf7b2ab8ae2dfbd84c434fba2499ed7c
NicoRosero <nicolas.nacho93@gmail.com>
2021-06-29 11:20:17 -0500

    game manager   objecttrigger
    aniadido singleton a gamemanager
    al tocar objeto desaparece

--- NeuroHelpAppGames/Assets/Scripts/Cancelacion/GameManager.cs
+++ NeuroHelpAppGames/Assets/Scripts/Cancelacion/GameManager.cs
@@ -3,41 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Test1 : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public Camera camera;
     public Sprite sprite;
+
+    private static GameManager sharedInstance;
 
     public GameObject[] imageObject;
     private GameObject randomSelected;
     string randomSelectedName;
     Sprite randomSelectedSprite;
-
+    int randomObjbuscados;
     public int[,] Grid;
     int vertical, horizontal, rows, columns;
-    
+
+   
+
     public Text topText;
     public Image topImage;
 
     //private  
 
+    private void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    public static GameManager GetInstance()
+    {
+        return sharedInstance;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-       
 
+        
         vertical = (int)camera.orthographicSize;
         horizontal = vertical * (Screen.width / Screen.height);
        
         imageObject = Resources.LoadAll<GameObject>("Prefabs/Cancelacion/ImagesPrefabs");
-        //imageObject[1].SetActive(false);
-        Debug.Log(Screen.width + " w " + Screen.height + " h " + horizontal + " hori " + vertical + " verti ");
         
         randomSelected = Instantiate(imageObject[Random.Range(0, 5)]);
         randomSelected.SetActive(false);
-        
-        Debug.Log(randomSelectedName);
-
         SetRandomSelectedImgage(randomSelected);
 
 
@@ -46,15 +55,18 @@ public class Test1 : MonoBehaviour
         rows = 4 ;
 
         Grid = new int[columns,rows];
-
-        for (int i=0; i<columns;i++)
+        while (randomObjbuscados <= 0)
         {
-            for (int j = 0; j < rows; j++)
+            for (int i = 0; i < columns; i++)
             {
-                Grid[i, j] = Random.Range(0,5);
-                Spawnfile(i, j, Grid[i, j]);
+                for (int j = 0; j < rows; j++)
+                {
+                    Grid[i, j] = Random.Range(0, 5);
+                    Spawnfile(i, j, Grid[i, j]);
+                }
             }
         }
+        Debug.Log(randomObjbuscados);
 
     }
     
@@ -68,15 +80,15 @@ public class Test1 : MonoBehaviour
     {
         
         GameObject g = Instantiate(imageObject[value]);
-        //Vector3 objectPosition = new Vector3(x - (horizontal - 0.5f), y - (vertical - 0.5f));
+        
         g.transform.position= new Vector3((x*2.5f)-(horizontal-0.5f), (y*2) - (vertical-1));
-        //g = Instantiate(imageObject[value]);
 
-        //var s = g.AddComponent<SpriteRenderer>();
-        //s.sprite = sprite;
-        //s.color = new Color(value,value,value);
-
+        if (g.name == randomSelected.name)
+        {
+            randomObjbuscados++;
+        }
     }
+
 
     private void SetRandomSelectedImgage(object randomObj)
     {
@@ -113,4 +125,7 @@ public class Test1 : MonoBehaviour
         topImage.sprite = randomSelectedSprite;
 
     }
+
+
+   
 }
---
+++ NeuroHelpAppGames/Assets/Scripts/Cancelacion/ObjectTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTrigger : MonoBehaviour
+{
+    string objName;
+    Collider2D collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+        // obj = GetComponent<>
+        objName = gameObject.name;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+
+            if (collider == touchedCollider)
+            {
+                gameObject.SetActive(false);
+                Debug.Log("haz tocado la pantalla en el obj: "+objName);
+            }
+
+        }
+    }
+}
