60d244edeac97f595f78eb96289e8422f25595c9
NicoRosero <nicolas.nacho93@gmail.com>
2021-09-15 18:40:14 -0500

    secure commit repeticion

---
+++ NeuroHelpAppGames/Assets/Scripts/Repeticion/GameManagerRepeticion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerRepeticion : MonoBehaviour
+{
+
+    public enum GameState
+    {
+        InicioImgGame, ImgGame, InicioVowelGame, VowelGame, InicioNumberGame, NumberGame, Final//, FinalScore
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
