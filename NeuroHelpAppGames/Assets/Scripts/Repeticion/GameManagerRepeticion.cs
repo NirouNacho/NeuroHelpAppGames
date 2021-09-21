fe3f349f3a537a9ea81a7e09eeb9df818183e4d5
NicoRosero <nicolas.nacho93@gmail.com>
2021-09-21 03:12:32 -0500

    debugging rep1

--- NeuroHelpAppGames/Assets/Scripts/Repeticion/GameManagerRepeticion.cs
+++ NeuroHelpAppGames/Assets/Scripts/Repeticion/GameManagerRepeticion.cs
@@ -13,7 +13,7 @@ public class GameManagerRepeticion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rep1.GetInstance().StartRep1Game();
     }
 
     // Update is called once per frame
--- NeuroHelpAppGames/Assets/Scripts/Repeticion/Rep1.cs
+++ NeuroHelpAppGames/Assets/Scripts/Repeticion/Rep1.cs
@@ -1,30 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Rep1 : MonoBehaviour
 {
+    //singleton
+    private static Rep1 sharedInstance;
 
     public GameObject[] arrayObject;
     private GameObject randomSelected;
     public GameObject auxDestroy;
-    
+
     public GameObject[] auxArray;
-    GameObject[][] auxVector;
-    GameObject[][] PrimeraFila;
-    GameObject[][] SegundaFila;
-    GameObject[][] TerceraFila;
-    GameObject[][] CuartaFila;
+    GameObject[,] auxVector;
+    GameObject[,] PrimeraFila;
+    GameObject[,] SegundaFila;
+    GameObject[,] TerceraFila;
+    GameObject[,] CuartaFila;
+
+    //singelton
+    private void Awake()
+    {
+        sharedInstance = this;
+
+        auxVector = new GameObject[2,9]; //quiero dos arrays de 9 slots
+
+
+
+    }
+    //singelton
+    public static Rep1 GetInstance()
+    {
+        return sharedInstance;
+    }
 
 
     public void StartRep1Game()
     {
         arrayObject = LlenarArrayObj("Prefabs/Repeticion");
+        
+        
+        Debug.Log("Entramos al random select");
         randomSelected = GetRandomSelected(arrayObject);
+      
+
+
         PrimeraFila = LlenarArrayFilas();
-
+        Debug.Log("primera fila");
+        ImprimirFila(PrimeraFila);
+        Debug.Log("impresos");
     }
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -47,28 +75,40 @@ public class Rep1 : MonoBehaviour
 
     public GameObject GetRandomSelected(GameObject[] array)
     {
-        return Instantiate(array[Random.Range(0, 9)]);
-
+        return Instantiate(array[UnityEngine.Random.Range(0, 9)]);
+        
     }
 
     //llena una fila con dos prefabas que se repiten y los otros y guarda el rabdom escogido
     //en [1][0]
-    public GameObject[][] LlenarArrayFilas()
+    public GameObject[,] LlenarArrayFilas()
     {
         GameObject auxObj;
-        auxVector[1][0] = GetRandomSelected(auxArray);
-        auxObj = auxVector[1][0];
-        bool aux = true;
+        auxObj = GetRandomSelected(arrayObject);
+        Debug.Log("random select auxObj " + auxObj);
+        auxVector[1,0] = auxObj;
+        //auxObj = auxVector[1,0];
+        Debug.Log("Vector auxVector[1,0] "+ auxVector[1, 0]);
 
         // lleno los dos que se repiten
         for (int i = 0; i <= 1; i++)
         {
 
-            int a = Random.Range(0, 4);
-            if(!auxVector[0][a].Equals(auxVector[1][0]))
+            int a = UnityEngine.Random.Range(0, 5);
+            Debug.Log("Random a] " + a);
+            try
             {
-                auxVector[0][a] = auxVector[1][0];
+                if ((!auxVector[0, a].Equals(auxVector[1, 0])))
+                {
+                    auxVector[0, i] = auxVector[1, 0];
+                    Debug.Log("Vector auxVector[0,a] " + auxVector[0, i]);
+                }
             }
+            catch (NullReferenceException ex)
+            {
+                Debug.Log("entra al null "+ ex);
+            }
+            
          
         }
 
@@ -76,12 +116,12 @@ public class Rep1 : MonoBehaviour
       
         for (int y = 0; y <= 4; y ++) //hago 5 veces
         {
-                int a = Random.Range(0, 9);
+                int a = UnityEngine.Random.Range(0, 9);
                 auxObj = arrayObject[a];
                 bool canIn = false;
                for(int x= 0;x<= 4; x++)//pregunto 5 veces 
                {
-                    if (auxObj.Equals(auxVector[0][x]))
+                    if (auxObj.Equals(auxVector[0,x]))
                     {
                         canIn = false;
                         break;
@@ -94,7 +134,7 @@ public class Rep1 : MonoBehaviour
            
                if (canIn)
                 {
-                    auxVector[0][y] = auxObj;
+                    auxVector[0,y] = auxObj;
                 }
             
         }
@@ -103,24 +143,25 @@ public class Rep1 : MonoBehaviour
     }
 
 
-    private void ImprimirFila(GameObject[][] aImprimir)
+    private void ImprimirFila(GameObject[,] aImprimir)
     {
 
+        for (int i = 0; i <= 4 ; i++)//5 colums
+        {
+            Spawnfile(i, 0, aImprimir);
+        }
     }
 
-    private void Spawnfile(int x, int y, int value, GameObject[][] aImprimir)
+    private void Spawnfile(int x, int y, GameObject[,] aImprimir)
     {
         int columns = 5;
         int rows = 4;
 
-        GameObject g = Instantiate(aImprimir[0][value]);
+        GameObject g = Instantiate(aImprimir[0,x]);
 
         g.transform.position = new Vector3((x * 2.5f) - (rows - 0.5f), (y * 2) - (columns - 1));
 
-        if (g.name == randomSelected.name)
-        {
-            randomObjbuscados++;
-        }
+       
     }
 
 
