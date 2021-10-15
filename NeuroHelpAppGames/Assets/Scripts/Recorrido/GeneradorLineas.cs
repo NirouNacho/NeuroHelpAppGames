8adcfa9f03afc9e8efa00590a7082354df920275
NicoRosero <nicolas.nacho93@gmail.com>
2021-10-15 01:57:04 -0500

    Inicio Game Recorrido

---
+++ NeuroHelpAppGames/Assets/Scripts/Recorrido/GeneradorLineas.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorLineas : MonoBehaviour
+{
+
+    public GameObject lineaGenerar;
+
+    Linea linea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 dondeGenerar = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, -1);
+            GameObject lineaActual = Instantiate(lineaGenerar, dondeGenerar, transform.rotation);
+            linea = lineaActual.GetComponent<Linea>();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            linea = null;
+        }
+
+        if (linea!=null)
+        {
+            Vector2 ratonPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            linea.DibujarLinea(ratonPos);
+        }
+
+
+    }
+}
---
+++ NeuroHelpAppGames/Assets/Scripts/Recorrido/Linea.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linea : MonoBehaviour
+{
+    LineRenderer linea;
+
+    List<Vector2> puntos;
+
+    Vector2 ultimoPunto;
+
+    private void Awake()
+    {
+        linea = GetComponent<LineRenderer>();
+    }
+
+    public void DibujarLinea(Vector2 ratonPos)
+    {
+        if(puntos == null)
+        {
+            puntos = new List<Vector2>();
+            DibujarPunto(ratonPos);
+            return;
+        }
+
+        if (Vector2.Distance(ultimoPunto,ratonPos)>= .05f)
+        {
+            DibujarPunto(ratonPos);
+        }
+
+
+    }
+
+
+
+    void DibujarPunto(Vector2 punto)
+    {
+        puntos.Add(punto);
+        linea.positionCount = puntos.Count;
+        linea.SetPosition(puntos.Count - 1, punto);
+        ultimoPunto = punto;
+    }
+
+}
