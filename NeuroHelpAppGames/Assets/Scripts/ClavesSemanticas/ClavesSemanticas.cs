50af1f48a4a4d554c7397c1af79fbb0bfcb0ce93
NicoRosero <nicolas.nacho93@gmail.com>
2021-12-08 02:09:34 -0500

    claves semanticas
    mostrar opciones en toggles

--- NeuroHelpAppGames/Assets/Scripts/ClavesSemanticas/ClavesSemanticas.cs
+++ NeuroHelpAppGames/Assets/Scripts/ClavesSemanticas/ClavesSemanticas.cs
@@ -7,7 +7,8 @@ public class ClavesSemanticas : MonoBehaviour
 {
     private static ClavesSemanticas sharedInstance;
 
-
+    int rand1,rand2 , rand3;
+    int randa, randb, randc;
     //Objetos
     public GameObject SearchObjPhrase;
 
@@ -49,9 +50,10 @@ public class ClavesSemanticas : MonoBehaviour
     //Botones
 
     public Button buttonOK;
+    public Button buttonComprobar;
 
     //Grupos de palabras
-    string[][] ArrayGroups;
+    //string[][] ArrayGroups;
     string[] Bebidas;
     string[] Ropa;
     string[] Animal;
@@ -64,11 +66,15 @@ public class ClavesSemanticas : MonoBehaviour
     string[] Herramienta;
     string[] Cuerpo;
 
+    List<KeyValuePair<string, string[]>> ArrayGroups;
+
+
     private void Awake()
     {
         sharedInstance = this;
+        ArrayGroups = new List<KeyValuePair<string, string[]>>();
         InitialziaTextArrays();
-
+           
     }
 
     public static ClavesSemanticas GetInstance()
@@ -80,6 +86,15 @@ public class ClavesSemanticas : MonoBehaviour
     {
         topText.text = "Mira, memoriza y recuerda que palabra se encontraba en cada grupo: ";
         SetAcFalseObjects();
+        Select3Random();
+    }
+
+    public void StartClavesSemanticasToggles()
+    {
+        topText.text = "Señala una palabra en cada grupo, de las que viste antes: ";
+        SetAcFalseObjects2ndPart();
+        Select3RandomToggle();
+
     }
 
     // Start is called before the first frame update
@@ -107,9 +122,10 @@ public class ClavesSemanticas : MonoBehaviour
         equiz2.SetActive(false);
         visto3.SetActive(false);
         equiz3.SetActive(false);
+        buttonComprobar.gameObject.SetActive(false);
     }
 
-    public void InitialziaTextArrays()
+        public void InitialziaTextArrays()
     {
         Bebidas= new string[] { "agua", "leche", "gaseosa" };
         Ropa = new string[] { "chaqueta", "pantalón", "camisa" };
@@ -120,14 +136,166 @@ public class ClavesSemanticas : MonoBehaviour
         Fruta = new string[] { "pera", "manzana", "piña" };
         Transporte = new string[] { "trole", "taxi", "bibicleta" };
         Color = new string[] { "rojo", "azul", "amarillo" };
-        Herramienta = new string[] { "martillo", "serrrucho", "destornillador" };
+        Herramienta = new string[] { "martillo", "serrucho", "destornillador" };
         Cuerpo = new string[] { "cabeza", "brazos", "piernas" };
-        ArrayGroups = new string[][]{Bebidas, Ropa, Animal, Mueble, Comida
-            , Deporte,Fruta,Transporte,Color,Herramienta,Cuerpo};
+        // ArrayGroups = new string[][]{Bebidas, Ropa, Animal, Mueble, Comida
+        //    , Deporte,Fruta,Transporte,Color,Herramienta,Cuerpo};
+
+        ArrayGroups.Add(new KeyValuePair<string, string[]>("Bebida", Bebidas));
+        ArrayGroups.Add(new KeyValuePair<string, string[]>("Animal", Animal));
+        ArrayGroups.Add(new KeyValuePair<string, string[]>("Mueble", Mueble));
+        ArrayGroups.Add(new KeyValuePair<string, string[]>("Comida", Comida));
+        ArrayGroups.Add(new KeyValuePair<string, string[]>("Deporte", Deporte));
+        ArrayGroups.Add(new KeyValuePair<string, string[]>("Fruta", Fruta));
+        ArrayGroups.Add(new KeyValuePair<string, string[]>("Transporte", Transporte));
+        ArrayGroups.Add(new KeyValuePair<string, string[]>("Color", Color));
+        ArrayGroups.Add(new KeyValuePair<string, string[]>("Herramienta", Herramienta));
+        ArrayGroups.Add(new KeyValuePair<string, string[]>("Parte del Cuerpo", Cuerpo));
+
     }
 
     private void Select3Random()
     {
+        //Select 3 random from array groups
 
+        rand1 = UnityEngine.Random.Range(0, 10);
+        rand2 = UnityEngine.Random.Range(0, 10);
+        while (rand1 == rand2)
+        {
+            rand2 = UnityEngine.Random.Range(0, 10);
+        }
+        rand3 = UnityEngine.Random.Range(0, 10);
+        while (rand1 == rand3 || rand2 == rand3)
+        {
+            rand3 = UnityEngine.Random.Range(0, 10);
+        }
+
+
+        //Poner el nombre de los grupos a las cabeceras
+
+        topImagePhrase1.text = ArrayGroups[rand1].Key;
+        topImagePhrase2.text = ArrayGroups[rand2].Key;
+        topImagePhrase3.text = ArrayGroups[rand3].Key;
+
+
+        //Poner el nombre del objeto buscado del grupo
+        
+        randa = UnityEngine.Random.Range(0, 3);
+        randb = UnityEngine.Random.Range(0, 3);
+        randc = UnityEngine.Random.Range(0, 3);
+
+        object1.text= ArrayGroups[rand1].Value[randa];
+        object2.text = ArrayGroups[rand2].Value[randb];
+        object3.text = ArrayGroups[rand3].Value[randc];
+
+
+        //If you would like to print the group name now, you can do:
+
+        // Debug.Log(groups[2].Key);
+        //If you want to access a group you can do:
+
+        // Debug.Log(groups[2].Value);
+        //If you would like to access a name in a group:
+
+        // Debug.Log(groups[2].Value[index]);
+    }
+
+
+    private void SetAcFalseObjects2ndPart()
+    {
+        object1.gameObject.SetActive(false); 
+        object2.gameObject.SetActive(false); 
+        object3.gameObject.SetActive(false);
+
+        ToggleGroup1.SetActive(true);
+        ToggleGroup2.SetActive(true);
+        ToggleGroup3.SetActive(true);
+        
+    }
+
+    private void Select3RandomToggle()
+    {
+        //Poner el nombre de los grupos a las cabeceras
+
+        topImagePhrase1.text = "¿Que " + ArrayGroups[rand1].Key + " era?";
+        topImagePhrase2.text = "¿Que " + ArrayGroups[rand2].Key + " era?";
+        topImagePhrase3.text = "¿Que " + ArrayGroups[rand3].Key + " era?";
+
+        
+
+        int posRand= UnityEngine.Random.Range(0, 2);
+        int positionDos = UnityEngine.Random.Range(0, 3);
+
+        //Toggle 1
+        if (posRand == 0)
+        {
+            ToggleText1A.text = ArrayGroups[rand1].Value[randa];
+            while(randa== positionDos)
+            {
+                positionDos = UnityEngine.Random.Range(0, 3);
+            }
+            ToggleText1B.text = ArrayGroups[rand1].Value[positionDos];
+        }
+        if (posRand == 1)
+        {
+            ToggleText1B.text = ArrayGroups[rand1].Value[randa];
+            while (randa == positionDos)
+            {
+                positionDos = UnityEngine.Random.Range(0, 3);
+            }
+            ToggleText1A.text = ArrayGroups[rand1].Value[positionDos];
+        }
+
+        //Toggle 2
+
+        posRand = UnityEngine.Random.Range(0, 2);
+        positionDos = UnityEngine.Random.Range(0, 3);
+
+        if (posRand == 0)
+        {
+            ToggleText2A.text = ArrayGroups[rand2].Value[randa];
+            while (randa == positionDos)
+            {
+                positionDos = UnityEngine.Random.Range(0, 3);
+            }
+            ToggleText2B.text = ArrayGroups[rand2].Value[positionDos];
+        }
+        if (posRand == 1)
+        {
+            ToggleText2B.text = ArrayGroups[rand2].Value[randa];
+            while (randa == positionDos)
+            {
+                positionDos = UnityEngine.Random.Range(0, 3);
+            }
+            ToggleText2A.text = ArrayGroups[rand2].Value[positionDos];
+        }
+
+        //Toggle 3
+        posRand = UnityEngine.Random.Range(0, 2);
+        positionDos = UnityEngine.Random.Range(0, 3);
+
+        if (posRand == 0)
+        {
+            ToggleText3A.text = ArrayGroups[rand3].Value[randa];
+            while (randa == positionDos)
+            {
+                positionDos = UnityEngine.Random.Range(0, 3);
+            }
+            ToggleText3B.text = ArrayGroups[rand3].Value[positionDos];
+        }
+        if (posRand == 1)
+        {
+            ToggleText3B.text = ArrayGroups[rand3].Value[randa];
+            while (randa == positionDos)
+            {
+                positionDos = UnityEngine.Random.Range(0, 3);
+            }
+            ToggleText3A.text = ArrayGroups[rand3].Value[positionDos];
+        }
+
+        buttonOK.gameObject.SetActive(false);
+        buttonComprobar.gameObject.SetActive(true);
     }
 }
+
+
